e777da5bf2ace1ff66d22a240d9e44bd619def86
AlahmadiQ8 <m91.alahmadi@gmail.com>
2018-08-23 17:58:34 -0700

    :sparkles: covered all es6 classes

--- index.js
+++ index.js
@@ -17,17 +17,80 @@ j.registerMethods({
   },
 })
 
+j.registerMethods(
+  {
+    build: function() {
+      return this.find(j.MethodDefinition)
+        .filter(path => path.value.key.name === 'constructor')
+        .find(j.FunctionExpression)
+    },
+  },
+  j.ClassDeclaration
+)
+
 const desugarClass = code => {
   return j(code)
     .find(j.ClassDeclaration)
     .forEach(path => {
       const className = path.node.id.name
-      const func = j.functionDeclaration(
-        j.identifier(className),
-        [],
-        j.blockStatement([])
-      )
-      j(path).replaceWith(func)
+      let declaration
+      let prototypes = []
+      j(path)
+        .find(j.MethodDefinition)
+        .forEach(path => {
+          const methodName = path.value.key.name
+          const isStatic = path.value.static
+          if (methodName === 'constructor') {
+            j(path)
+              .find(j.FunctionExpression)
+              .forEach(path => {
+                declaration = j.functionDeclaration(
+                  j.identifier(className),
+                  path.value.params,
+                  path.value.body
+                )
+              })
+          } else {
+            j(path)
+              .find(j.FunctionExpression)
+              .forEach(path => {
+                const method = j.functionExpression(
+                  null,
+                  path.value.params,
+                  path.value.body
+                )
+                const prototype = j.expressionStatement(
+                  j.assignmentExpression(
+                    '=',
+                    j.memberExpression(
+                      !isStatic
+                        ? j.memberExpression(
+                            j.identifier(className),
+                            j.identifier('prototype'),
+                            false
+                          )
+                        : j.identifier(className),
+                      j.identifier(methodName),
+                      false
+                    ),
+                    method
+                  )
+                )
+                prototypes.push(prototype)
+              })
+          }
+        })
+      if (!declaration) {
+        declaration = j.functionDeclaration(
+          j.identifier(className),
+          [],
+          j.blockStatement([])
+        )
+      }
+      j(path).replaceWith(declaration)
+      while (prototypes.length > 0) {
+        j(path).insertAfter(prototypes.pop())
+      }
     })
     .toSource()
 }
--- index.test.js
+++ index.test.js
@@ -21,3 +21,146 @@ function Omg() {}
 `
   )
 })
+
+it('preserve constructor into function declaration', () => {
+  buildCase(
+    `
+class Test {
+  constructor() {
+    const a = 1
+  }
+}
+
+class Omg {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+}
+`,
+    `
+function Test() {
+  const a = 1
+}
+
+function Omg(x, y) {
+  this.x = x
+  this.y = y
+}
+`
+  )
+})
+
+it('converts methods to prototype declarations', () => {
+  buildCase(
+    `
+class Test {
+  constructor() {
+    const a = 1
+  }
+
+  run() {
+    const foo = () => 'phiphi'
+  }
+}
+
+class Omg {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  add(x, y) {
+    return x + y
+  }
+
+  substract(x, y) {
+    return x - y
+  }
+}
+`,
+    `
+function Test() {
+  const a = 1
+}
+
+Test.prototype.run = function() {
+  const foo = () => 'phiphi'
+};
+
+function Omg(x, y) {
+  this.x = x
+  this.y = y
+}
+
+Omg.prototype.add = function(x, y) {
+  return x + y
+};
+
+Omg.prototype.substract = function(x, y) {
+  return x - y
+};
+`
+  )
+})
+
+it('converts static methods to prototype declarations', () => {
+  buildCase(
+    `
+class Test {
+  constructor() {
+    const a = 1
+  }
+
+  run() {
+    const foo = () => 'phiphi'
+  }
+}
+
+class Omg {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  static multip(x, y) {
+    return x * y
+  }
+
+  add(x, y) {
+    return x + y
+  }
+
+  substract(x, y) {
+    return x - y
+  }
+}
+`,
+    `
+function Test() {
+  const a = 1
+}
+
+Test.prototype.run = function() {
+  const foo = () => 'phiphi'
+};
+
+function Omg(x, y) {
+  this.x = x
+  this.y = y
+}
+
+Omg.multip = function(x, y) {
+  return x * y
+};
+
+Omg.prototype.add = function(x, y) {
+  return x + y
+};
+
+Omg.prototype.substract = function(x, y) {
+  return x - y
+};
+`
+  )
+})
